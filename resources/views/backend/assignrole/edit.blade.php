435543c46268b2256e08a9ae2bf41c5050b57a62
LeartIstrefaj <leartistrefaj02@gmail.com>
2023-02-06 20:57:24 +0100

    other comit

--- resources/views/backend/assignrole/edit.blade.php
+++ resources/views/backend/assignrole/edit.blade.php
@@ -8,14 +8,14 @@
                 <h2 class="text-gray-700 uppercase font-bold">Edit User &amp; Role</h2>
             </div>
             <div class="flex flex-wrap items-center">
-                <a href="{{ route('assignrole.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
+                <a href="{{ route('assignrole.index') }}" class="bg-color-3 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Back to User and Assign Role</span>
                 </a>
             </div>
         </div>
 
-        <div class="table w-full mt-8 bg-white rounded">
+        <div class="table  w-full mt-8 bg-color rounded">
             <form action="{{ route('assignrole.update',$user->id) }}" method="POST" class="w-full max-w-lg px-6 py-12">
                 @csrf
                 @method('PUT')
@@ -73,8 +73,8 @@
                 </div>
                 <div class="md:flex md:items-center">
                     <div class="md:w-1/3"></div>
-                    <div class="md:w-2/3">
-                        <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                    <div class="md:w-2/3 flex justify-center">
+                        <button class="btn-2 shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                             Update User
                         </button>
                     </div>
