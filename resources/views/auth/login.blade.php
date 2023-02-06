4443b8af7d2f74114ed0dbea06f095174471c3c7
LeartIstrefaj <leartistrefaj02@gmail.com>
2023-02-06 10:54:14 +0100

    init in admin dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <label class="block text-gray-700 text-sm font-bold mb-2" for="emailaddress">
                 Email
             </label>
-            <input class="input shadow appearance-none border @error('password') border-red-500 @enderror rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" id="emailaddress" placeholder="info@example.com">
+            <input class="input  @error('password')  @enderror  w-full py-2 px-3 text-gray-700 mb-3 leading-tight " type="email" name="email" id="emailaddress" placeholder="info@example.com">
             @error('email')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
             @enderror
@@ -18,7 +18,7 @@
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                 Password
             </label>
-            <input class="input shadow appearance-none border @error('password') border-red-500 @enderror rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" id="password" placeholder="********">
+            <input class="input  @error('password')  @enderror  w-full py-2 px-3 text-gray-700 mb-3 leading-tight " type="password" name="password" id="password" placeholder="********">
             @error('password')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
             @enderror
--- resources/views/backend/classes/create.blade.php
+++ resources/views/backend/classes/create.blade.php
@@ -8,14 +8,14 @@
                 <h2 class="text-gray-700 uppercase font-bold">Create Class</h2>
             </div>
             <div class="flex flex-wrap items-center">
-                <a href="{{ route('classes.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
+                <a href="{{ route('classes.index') }}" class="bg-color-3 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Back to Class</span>
                 </a>
             </div>
         </div>
 
-        <div class="table w-full mt-8 bg-white rounded">
+        <div class="flex justify-center table w-full mt-8 bg-color rounded">
             <form action="{{ route('classes.store') }}" method="POST" class="w-full max-w-xl px-6 py-12">
                 @csrf
                 <div class="md:flex md:items-center mb-6">
@@ -83,8 +83,8 @@
 
                 <div class="md:flex md:items-center">
                     <div class="md:w-1/3"></div>
-                    <div class="md:w-2/3">
-                        <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                    <div class="md:w-2/3 flex justify-center">
+                        <button class="btn-2 shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                             Create Class
                         </button>
                     </div>
